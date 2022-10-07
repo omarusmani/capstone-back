f280ef8e9223eff18467463fc0dc485b833b5a9c
omarusmani <omarusmani3@hotmail.com>
2022-10-07 15:07:53 -0500

    fix

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const cors= require('cors');
-const userrouter= require("./routes/userrouter");
-const postrouter= require("./routes/postrouter");
+// const userrouter= require("./routes/userrouter");
+// const postrouter= require("./routes/postrouter");
 const login = require("./routes/login")
 
 
 
 app.use(cors());
 app.use(express.json());
-app.use("/user", userrouter);
-app.use("/post", postrouter);
+// app.use("/user", userrouter);
+// app.use("/post", postrouter);
 app.use("/login",login);
-
 
 
 
